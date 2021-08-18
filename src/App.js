abd8a320337ae905ba56efe7097edc05df59d488
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2021-08-18 11:27:46 -0300

    coin filter using search input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import { useEffect, useState } from 'react';
 import List from './components/List';
+import Search from './components/Search';
 
 const App = () => {
-  const [coinList, setCoinList] = useState('');
+  const [coinList, setCoinList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
     )
       .then((response) => response.json())
-      .then((data) => setCoinList(data));
+      .then((data) => {
+        const modifiedList = data.map((coin) => {
+          coin.on_watch_list = false;
+          return coin;
+        });
+        setCoinList(modifiedList);
+      });
   }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedCoins = coinList.filter((coin) =>
+    coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <h1> Coins lis</h1>
-      {coinList.length ? <List coinList={coinList} /> : <p>Cargando...</p>}
+      <h1> Coins list {searchTerm}</h1>
+      <Search search={searchTerm} onSearch={handleSearch} />
+      {coinList.length ? <List coinList={searchedCoins} /> : <p>Cargando...</p>}
     </div>
   );
 };
